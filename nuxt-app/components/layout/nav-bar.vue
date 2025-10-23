e8836adf347e8415c446753a1aaaea32ffc76fab
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-23 15:50:24 +0200

    updating style in navbar component

--- nuxt-app/components/layout/nav-bar.vue
+++ nuxt-app/components/layout/nav-bar.vue
@@ -1,26 +1,26 @@
 <template>
 
-    <nav class="p-4 bg-blue-500 text-white flex flex-row">
+    <nav class="p-4 bg-slate-800 text-white flex flex-row">
 
-        <div class="basis-2xs text-lg">
+        <div class="basis-2xs p-2 text-lg">
             <p><a href="/">Dylan Tettarasar</a></p>
         </div>
 
         <div class="basis-full flex flex row content-center justify-end">
 
-            <div class="mx-8">
+            <div class="mx-8 p-2 rounded-md hover:bg-sky-700">
                 <p><a href="/">Home</a></p>
             </div>
 
-            <div class="mx-8">
+            <div class="mx-8 p-2 rounded-md hover:bg-sky-700">
                 <p><a href="/resume">Resume</a></p>
             </div>
 
-            <div class="mx-8">
+            <div class="mx-8 p-2 rounded-md hover:bg-sky-700">
                 <p><a href="/portfolio">Portfolio</a></p>
             </div>
 
-            <div class="mx-8">
+            <div class="mx-8 p-2 rounded-md hover:bg-sky-700">
                 <p><a href="/contact">Contact</a></p>
             </div>
 
