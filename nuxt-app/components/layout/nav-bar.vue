2fad017207f3a8df6d9e573978c0da2fc1dff62e
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-23 12:29:18 +0200

    building navbar component

--- nuxt-app/components/layout/nav-bar.vue
+++ nuxt-app/components/layout/nav-bar.vue
@@ -1,6 +1,34 @@
 <template>
 
-    <p>This is the navbar component</p>
+    <nav class="p-4 bg-blue-500 text-white flex flex-row">
+
+        <div class="basis-2xs text-lg">
+            Dylan Tettarasar
+        </div>
+
+        <div class="basis-full flex flex row content-center justify-end">
+
+            <div class="mx-8">
+                <p>Home</p>
+            </div>
+
+            <div class="mx-8">
+                <p>Resume</p>
+            </div>
+
+            <div class="mx-8">
+                <p>Portfolio</p>
+            </div>
+
+            <div class="mx-8">
+                <p>Contact</p>
+            </div>
+
+        </div>
+
+    </nav>
+
+    
 
 </template>
 
