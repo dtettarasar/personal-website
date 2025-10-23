abfd3b74e181512c4cddd225e6748d8494ae93f7
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-23 12:38:16 +0200

    add link in navbar component

--- nuxt-app/components/layout/nav-bar.vue
+++ nuxt-app/components/layout/nav-bar.vue
@@ -9,19 +9,19 @@
         <div class="basis-full flex flex row content-center justify-end">
 
             <div class="mx-8">
-                <p>Home</p>
+                <p><a href="/">Home</a></p>
             </div>
 
             <div class="mx-8">
-                <p>Resume</p>
+                <p><a href="/resume">Resume</a></p>
             </div>
 
             <div class="mx-8">
-                <p>Portfolio</p>
+                <p><a href="/portfolio">Portfolio</a></p>
             </div>
 
             <div class="mx-8">
-                <p>Contact</p>
+                <p><a href="/contact">Contact</a></p>
             </div>
 
         </div>
