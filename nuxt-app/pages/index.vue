9e3a4131aa58a09e93f40666e2ff535faee91e9a
Dylan Tettarasar <dtettarasar@gmail.com>
2025-10-31 12:49:44 +0100

    homepage: update font size

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -8,7 +8,7 @@
 
       <div class="flex flex-col gap-4">
 
-        <div class="justify-center border-green-400 md:text-4xl flex flex-row items-center gap-4">
+        <div class="justify-center border-green-400 text-2xl md:text-4xl flex flex-row items-center gap-4">
 
           <Icon name="mdi:account-circle" class="w-10 h-10 text-green-400" />
           <h2 class="text-green-400 drop-shadow-[0_0_15px_rgba(74,222,128,0.4)]">About me</h2>
@@ -24,28 +24,28 @@
 
     <div class="container p-4 mx-auto text-white md:text-lg">
 
-      <p class="my-1 md:my-4">
+      <p class="my-2 md:my-4">
 
         I started my career in <strong>digital communication and web project management</strong>, spending several years leading projects that connected strategy, 
         creativity, and technology.
 
       </p>
 
-      <p class="my-1 md:my-4">
+      <p class="my-2 md:my-4">
 
         During this time, I worked on <strong>WordPress websites, SEO optimization, and web content production</strong>, 
         often taking on both the strategic vision and the hands-on execution — from UX design to publishing and performance analysis.
 
       </p>
 
-      <p class="my-1 md:my-4">
+      <p class="my-2 md:my-4">
 
         This experience gave me a <strong>360° understanding of the web ecosystem</strong>: how a website is designed, developed, optimized, and maintained over time. 
         It also shaped my sense of autonomy, creativity, and adaptability — qualities that have always guided my work.
 
       </p>
 
-      <p class="my-1 md:my-4">
+      <p class="my-2 md:my-4">
 
         Over time, I felt the need to go deeper into the <strong>technical side of creation</strong>, to understand what happens under the hood — beyond CMS tools and pre-built solutions.
         That’s what led me to learn <strong>JavaScript, Python, and modern frameworks like Nuxt.js, Vue, and Django</strong>, and to gradually move towards <strong>fullstack development</strong>.
@@ -60,7 +60,7 @@
 
       <div class="flex flex-col gap-4">
 
-        <div class="justify-center border-green-400 md:text-4xl flex flex-row items-center gap-4">
+        <div class="justify-center border-green-400 text-2xl md:text-4xl flex flex-row items-center gap-4">
 
           <Icon name="mdi:comment-account" class="w-10 h-10 text-green-400" />
           <h2 class="text-green-400 drop-shadow-[0_0_15px_rgba(74,222,128,0.4)]">Contact</h2>
