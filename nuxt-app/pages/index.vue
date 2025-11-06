36bd5913daac76bf8a13a7af752b7bf5f3c21864
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-06 12:31:45 +0100

    building design for the cta

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -38,21 +38,10 @@
 
       <div>
 
-        <NuxtLink to="/resume">
-          
-          <div>
-            Resume
-          </div>
-
-        </NuxtLink>
-
-        <NuxtLink to="/portfolio">
-          
-          <div>
-            Portfolio
-          </div>
-
-        </NuxtLink>
+        <NuxtLink class="w-fit p-4 flex flex-col items-center bg-slate-800 border-4 border-emerald-400 rounded-lg hover:bg-emerald-400 hover:shadow-[0_0_15px_rgba(74,222,128,0.6)] md:text-2xl lg:text-3xl text-white hover:text-black transition-all duration-300" to="/resume">
+          <Icon name="mdi:mail-ru" class="w-10 h-10" />
+          Resume
+        </NuxtLink>        
 
       </div>
 
