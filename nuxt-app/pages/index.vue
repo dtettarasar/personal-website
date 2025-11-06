9a7b20ca17491c5467ea6d17522feecedc2cfa5b
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-06 12:55:50 +0100

    build the containers for the cta on index page

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -36,12 +36,17 @@
 
       </p>
 
-      <div>
+      <div class="flex flex-row justify-around pt-4">
 
         <NuxtLink class="w-fit p-4 flex flex-col items-center bg-slate-800 border-4 border-emerald-400 rounded-lg hover:bg-emerald-400 hover:shadow-[0_0_15px_rgba(74,222,128,0.6)] md:text-2xl lg:text-3xl text-white hover:text-black transition-all duration-300" to="/resume">
           <Icon name="mdi:mail-ru" class="w-10 h-10" />
           Resume
-        </NuxtLink>        
+        </NuxtLink>     
+        
+        <NuxtLink class="w-fit p-4 flex flex-col items-center bg-slate-800 border-4 border-emerald-400 rounded-lg hover:bg-emerald-400 hover:shadow-[0_0_15px_rgba(74,222,128,0.6)] md:text-2xl lg:text-3xl text-white hover:text-black transition-all duration-300" to="/resume">
+          <Icon name="mdi:mail-ru" class="w-10 h-10" />
+          Resume
+        </NuxtLink>     
 
       </div>
 
