37b9145e6df4a1168270833fda1695a701d9634e
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-08 12:00:29 +0100

    hero small: building design

--- nuxt-app/components/sections/hero-small.vue
+++ nuxt-app/components/sections/hero-small.vue
@@ -1,11 +1,36 @@
 <template>
 
-    <div>
-        This is the hero small component
-    </div>
+    <section class="bg-indigo-500 flex flex-col justify-center items-center py-5">
+
+        <div class="flex flex-col items-center">
+            
+            <Icon :name="icon" class="w-10 h-10 text-white text-2xl md:text-3xl" />
+
+            <h1 class="text-3xl md:text-5xl" >Resume</h1>
+
+        </div>
+
+    </section>
 
 </template>
 
+<style scoped>
+
+
+
+</style>
+
 <script setup>
+
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    default: 'mdi:star' // icône par défaut si aucune n’est passée
+  }
+})
 
 </script>
