8882341ed41c5a911b09fc8ca04bf87811bcfb1c
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-08 12:40:03 +0100

    footer: change text color

--- nuxt-app/components/layout/footer.vue
+++ nuxt-app/components/layout/footer.vue
@@ -4,7 +4,7 @@
 
         <div class="container py-4 mx-auto flex flex col justify-center">
 
-            <p class="text-green-400 drop-shadow-[0_0_15px_rgba(74,222,128,0.4)] md:text-xl">Website created by Dylan Tettarasar © 2025</p>
+            <p class="text-emerald-400 drop-shadow-[0_0_15px_rgba(74,222,128,0.4)] md:text-xl">Website created by Dylan Tettarasar © 2025</p>
 
         </div>
 
