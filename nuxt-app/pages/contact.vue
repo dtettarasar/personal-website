2f5d47cbcc2d1677e3ebd0a421c1aa6e4e30bf7a
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-19 11:14:22 +0100

    add the teasing component in contact and portfolio pages

--- nuxt-app/pages/contact.vue
+++ nuxt-app/pages/contact.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
+  <div class="bg-slate-800">
     
     <sections-hero-small icon="mdi:comment-account" title="Contact" ></sections-hero-small>
+
+    <other-teasing></other-teasing>
 
   </div>
 </template>
--- nuxt-app/pages/portfolio.vue
+++ nuxt-app/pages/portfolio.vue
@@ -1,7 +1,9 @@
 <template>
-  <div>
+  <div class="bg-slate-800">
 
     <sections-hero-small icon="mdi:application-braces" title="Portfolio" ></sections-hero-small>
+
+    <other-teasing></other-teasing>
 
   </div>
 </template>
