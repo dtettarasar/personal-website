dc6190f07338e3f594147e7542ebd7319b5d2e99
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-24 16:08:24 +0100

    footer: update content

--- nuxt-app/components/layout/footer.vue
+++ nuxt-app/components/layout/footer.vue
@@ -2,9 +2,16 @@
 
     <div class="bg-slate-800">
 
-        <div class="container py-4 mx-auto flex flex col justify-center">
+        <div class="container py-4 mx-auto flex justify-center">
 
             <p class="text-emerald-400 drop-shadow-[0_0_15px_rgba(74,222,128,0.4)] md:text-xl">Website created by Dylan Tettarasar © 2025</p>
+
+        </div>
+
+        <div class="flex flex-col pt-2 pb-8 justify-center items-center">
+
+            <p class="text-center text-gray-400 mb-2">Built with</p>
+            <a target="_blank" href="https://nuxt.com/"><img class="w-[120px]" src="/nuxt-logo-green-white.svg"></img></a>
 
         </div>
 
