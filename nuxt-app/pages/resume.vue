72282be45324e918f11c98b6696650688a04f587
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-26 10:56:47 +0100

    building section title for resume page

--- nuxt-app/pages/resume.vue
+++ nuxt-app/pages/resume.vue
@@ -8,6 +8,22 @@
     <!--<other-test-mongo-db></other-test-mongo-db>-->
 
   </div>
+
+  <div class="container p-4 mx-auto text-white md:text-lg">
+
+    <div class="bg-slate-800 p-6 rounded-lg flex flex-row items-center mb-6 gap-2">
+
+      <h2 class="text-white">Experiences</h2>
+
+      <div class="bg-white h-[4px] w-full rounded-lg ">
+
+      </div>
+
+    </div>
+
+  </div>
+
+
 </template>
 
 <script setup lang="ts">
