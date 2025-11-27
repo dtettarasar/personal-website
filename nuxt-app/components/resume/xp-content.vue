8bd8c6e34ab15f127f73fc968fa20267a0e6c4e9
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-27 11:43:00 +0100

    experience component : building an alt card design

--- nuxt-app/components/resume/xp-content.vue
+++ nuxt-app/components/resume/xp-content.vue
@@ -8,7 +8,7 @@
                 <img class="object-contain" src="/img/resume/experiences/company-logo-dii.png">
             </div>
 
-            <div class='flex-1 min-w-0 bg-slate-700 text-white p-8 rounded-lg shadow-lg'>
+            <div class='flex-1 min-w-0 p-8 rounded-lg'>
 
                 <div class="xp-company">
                     <p class="text-2xl md:text-4xl" >Drive Innovation Insights (Paris, France)</p>
@@ -18,23 +18,52 @@
 
                     <p class="">Webmaster</p>
 
-                    <span>•</span>
+                    <span class="text-emerald-400">•</span>
 
                     <p>January 2018 - Present</p>
 
                 </div>
 
                 <div>
-                    <ul>
+                    <ul class="md:text-xl">
+
                         <li>websites creation and content management, using WordPress</li>
                         <li>graphic design (photo-editing, banners, visual elements for social media...)</li>
                         <li>stats monitoring in Google Analytics</li>
                         <li>Salesforce administration and internal support</li>
+
                     </ul>
                 </div>
 
             </div>
 
+        </div>
+
+    </div>
+
+
+
+    <div class="max-w-lg overflow-hidden bg-white shadow-sm border border-slate-200 rounded-lg">
+
+        <div class="p-4">
+            <img class="w-[50%] mx-auto" src="/img/resume/experiences/company-logo-dii.png"></img>
+        </div>
+        <div class="px-6 py-4">
+            <p class="text-xl md:text-2xl mb-2" >Drive Innovation Insights (Paris, France)</p>
+
+            <div class="flex flex-row gap-3 text-l md:text-xl font-light ">
+
+                <p class="">Webmaster</p>
+
+                <span class="text-emerald-400">•</span>
+
+                <p>January 2018 - Present</p>
+
+            </div>
+            
+            <p class="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+            </p>
         </div>
 
     </div>
