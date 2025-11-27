24d2cccfee29332898d253bde2c0979c60f0ac26
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-27 11:18:08 +0100

    resume: update on experience component

--- nuxt-app/components/resume/xp-content.vue
+++ nuxt-app/components/resume/xp-content.vue
@@ -4,19 +4,22 @@
 
         <div class='flex flex-col md:flex-row gap-4 md:gap-8 w-full'>
 
-            <div class='w-full md:w-64 flex-none'>
-                <img class="mx-auto md:mx-0" src="https://dtettarasar.github.io/img/resume/xp/company-logo-dii.png">
+            <div class='flex items-center justify-center md:w-1/4'>
+                <img class="object-contain" src="/img/resume/experiences/company-logo-dii.png">
             </div>
 
-            <div class='flex-1 min-w-0'>
+            <div class='flex-1 min-w-0 bg-slate-700 text-white p-8 rounded-lg shadow-lg'>
 
                 <div class="xp-company">
-                    <p class="break-words" >Drive Innovation Insights (Paris, France)</p>
+                    <p class="text-2xl md:text-4xl" >Drive Innovation Insights (Paris, France)</p>
                 </div>
 
-                <div class="xp-title">
+                <div class="flex flex-row gap-3 text-xl md:text-2xl font-light my-4 ">
 
-                    <p class="xp-job-name">Webmaster</p>
+                    <p class="">Webmaster</p>
+
+                    <span>•</span>
+
                     <p>January 2018 - Present</p>
 
                 </div>
