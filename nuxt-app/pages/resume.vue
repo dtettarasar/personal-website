1c004f8a6cd4515b141a4d6a79060d73cc7aae8e
Dylan Tettarasar <dtettarasar@gmail.com>
2025-11-28 12:11:59 +0100

    resume page: pass the experiences data from a new array to the component. Render all the exprience with v for loop

--- nuxt-app/pages/resume.vue
+++ nuxt-app/pages/resume.vue
@@ -14,22 +14,12 @@
   <div class="container p-4 mx-auto flex flex-row gap-8 flex-wrap justify-center"> 
 
     <resume-xp-content
-      companyName="Drive Innovation Insights"
-      companyVenue="Paris, France"
-      jobTitle="Webmaster"
-      :jobMissions="['test1', 'test2']"
-    ></resume-xp-content>
-
-    <resume-xp-content
-      companyName="Drive Innovation Insights"
-      companyVenue="Paris, France"
-      jobTitle="Web Project Manager"
-    ></resume-xp-content>
-
-    <resume-xp-content
-      companyName="Drive Innovation Insights"
-      companyVenue="Paris, France"
-      jobTitle="CRM Manager"
+      v-for="(xp, index) in experiences"
+      :key="index"
+      :companyName="xp.companyName"
+      :companyVenue="xp.companyVenue"
+      :jobTitle="xp.jobTitle"
+      :jobMissions="xp.jobMissions"
     ></resume-xp-content>
 
   </div>
@@ -45,5 +35,39 @@
 </template>
 
 <script setup lang="ts">
+
+  const experiences = [
+    {
+      companyName: "Drive Innovation Insights",
+      companyVenue: "Paris, France",
+      jobTitle: "Webmaster",
+      jobMissions: [
+        "websites creation and content management, using WordPress",
+        "graphic design (photo-editing, banners, visual elements for social media...)",
+        "stats monitoring in Google Analytics",
+        "Salesforce administration and internal support"
+      ]
+    },
+    {
+      companyName: "Drive Innovation Insights",
+      companyVenue: "Paris, France",
+      jobTitle: "Web Project Manager",
+      jobMissions: [
+        "project management for clients' websites creation",
+        "coordination between clients, developers and designers",
+        "ensuring deadlines and quality standards are met"
+      ]
+    },
+    {
+      companyName: "Drive Innovation Insights",
+      companyVenue: "Paris, France",
+      jobTitle: "CRM Manager",
+      jobMissions: [
+        "managing and optimizing the company's CRM system",
+        "training staff on CRM best practices",
+        "analyzing customer data to improve marketing strategies"
+      ]
+    }
+  ]
 
 </script>
