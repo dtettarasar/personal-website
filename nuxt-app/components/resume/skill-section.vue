c10ca232bfb87eae08c4f7c0cda27c7fd24a0c8a
Dylan Tettarasar <dtettarasar@gmail.com>
2025-12-02 14:37:47 +0100

    skill section: add animated background

--- nuxt-app/components/resume/skill-section.vue
+++ nuxt-app/components/resume/skill-section.vue
@@ -1,9 +1,40 @@
-<script>
+<script setup lang="ts">
+
+    const containerStyleClasses = [
+        'container',
+        'p-4',
+        'mx-auto',
+    ]
 
 </script>
 
 <template>
 
-    <p>this is the skill section component</p>
+    <div :class="containerStyleClasses">
+
+        <div id="skill-container" class="animate-gradient-move rounded-lg h-[500px]" ></div>
+        
+    </div>
 
 </template>
+
+<style>
+
+/*
+ * Nous laissons le gradient CSS complexe ici, car il est difficile
+ * de le recréer avec les utilitaires de base de Tailwind.
+ * En le laissant ici, il est SCOPED et géré localement.
+ */
+#skill-container {
+  background: linear-gradient(
+    135deg,
+    #0d1117,
+    #1c2834,
+    #223844,
+    #234d4d,
+    #3aa37f
+  );
+  background-size: 400% 400%;
+}
+
+</style>
