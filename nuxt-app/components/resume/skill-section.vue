6590813e1baa9e21fc15987bc7885f397332a09f
Dylan Tettarasar <dtettarasar@gmail.com>
2025-12-02 14:59:33 +0100

    skill section: added 2 subsections

--- nuxt-app/components/resume/skill-section.vue
+++ nuxt-app/components/resume/skill-section.vue
@@ -6,13 +6,51 @@
         'mx-auto',
     ]
 
+    const separatorClasses = [
+
+        'w-[70%]',
+        'h-[4px]',
+        'rounded-full',
+        'bg-white',
+        'shadow-[0_0_10px_rgba(255,255,255,0.6)]',
+        'mx-auto',
+        'my-2',
+
+    ]
+
 </script>
 
 <template>
 
     <div :class="containerStyleClasses">
 
-        <div id="skill-container" class="animate-gradient-move rounded-lg h-[500px]" ></div>
+        <div id="skill-container" class="animate-gradient-move rounded-lg h-[500px]" >
+
+            <div class="flex flex-col md:flex-row items-center justify-center gap-2 py-4">
+
+                <Icon 
+                    name="mdi:star" 
+                    class="size-[30px] md:size-[40px] lg:size-[60px] text-white" 
+                />
+
+                <p class="text-white text-lg md:text-xl lg:text-2xl flex-shrink-0">Computing</p>
+
+            </div>
+
+            <div :class="separatorClasses"></div>
+
+            <div class="flex flex-col md:flex-row items-center justify-center gap-2 py-4">
+
+                <Icon 
+                    name="mdi:star" 
+                    class="size-[30px] md:size-[40px] lg:size-[60px] text-white" 
+                />
+
+                <p class="text-white text-lg md:text-xl lg:text-2xl flex-shrink-0">Language</p>
+
+            </div>
+
+        </div>
         
     </div>
 
