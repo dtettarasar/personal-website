7cd639fd81525751a11a4901cd58cd51ad93dd63
Dylan Tettarasar <dtettarasar@gmail.com>
2025-12-04 17:24:10 +0100

    update footer: add credits for icons

--- nuxt-app/components/layout/footer.vue
+++ nuxt-app/components/layout/footer.vue
@@ -1,20 +1,60 @@
 <template>
 
-    <div class="bg-slate-800">
+  <div class="bg-slate-800 py-4">
 
-        <div class="container py-4 mx-auto flex justify-center">
-
-            <p class="text-emerald-400 drop-shadow-[0_0_15px_rgba(74,222,128,0.4)] md:text-xl">Website created by Dylan Tettarasar © 2025</p>
-
-        </div>
-
-        <div class="flex flex-col pt-2 pb-8 justify-center items-center">
-
-            <p class="text-center text-gray-400 mb-2">Built with</p>
-            <a target="_blank" href="https://nuxt.com/"><img class="w-[120px]" src="/nuxt-logo-green-white.svg"></img></a>
-
-        </div>
-
+    <!-- Text & owner -->
+    <div class="container py-4 mx-auto flex justify-center">
+      <p class="text-emerald-400 drop-shadow-[0_0_15px_rgba(74,222,128,0.4)] md:text-xl">
+        Website created by Dylan Tettarasar © 2025
+      </p>
     </div>
+
+    <!-- Built with -->
+    <div class="flex flex-col pt-2 pb-4 justify-center items-center">
+      <p class="text-center text-gray-400 mb-2">Built with</p>
+      <a target="_blank" href="https://nuxt.com/">
+        <img class="w-[120px]" src="/nuxt-logo-green-white.svg" />
+      </a>
+    </div>
+
+    <!-- Icon credits -->
+    <div class="pb-6 px-6 text-center text-xs text-gray-500 leading-relaxed">
+      <p class="mb-1">Icon Credits:</p>
+
+      <p>
+        <a 
+          href="https://www.flaticon.com/fr/icones-gratuites/boulangerie" 
+          title="boulangerie icônes"
+          class="underline hover:text-emerald-400"
+          target="_blank"
+        >
+          Boulangerie icônes créées par cahụụngung – Flaticon
+        </a>
+      </p>
+
+      <p>
+        <a 
+          href="https://www.flaticon.com/free-icons/cup-of-tea" 
+          title="cup of tea icons"
+          class="underline hover:text-emerald-400"
+          target="_blank"
+        >
+          Cup of tea icons created by Icon Home – Flaticon
+        </a>
+      </p>
+
+      <p class="mt-2">
+        Other icons provided by 
+        <a 
+          href="https://icones.js.org/" 
+          target="_blank" 
+          class="underline hover:text-emerald-400"
+        >
+          icones.js.org
+        </a>
+      </p>
+    </div>
+
+  </div>
 
 </template>
