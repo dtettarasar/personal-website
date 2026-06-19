4ae42b4f975560f23eac460a50050ca3aee0c61f
Dylan Tettarasar <dtettarasar@gmail.com>
2026-06-19 13:18:46 +0200

    update education section component

--- nuxt-app/components/resume/edu-section.vue
+++ nuxt-app/components/resume/edu-section.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
 
+    import { computed } from 'vue'
+    import { useI18n } from 'vue-i18n'
     import { useEducationsStore } from '@/stores/educationsStore'
 
+    const { locale } = useI18n()
     const educationsStore = useEducationsStore();
-    await useAsyncData('educations', () => educationsStore.fetchData())
+    await useAsyncData('educations', () => educationsStore.fetchData(locale.value), {
+        watch: [locale]
+    })
+
+    // 4. AJUSTEMENT : La propriété calculée qui va chercher le bon tableau dans le dictionnaire Pinia
+    const educations = computed(() => educationsStore.dataByLocale[locale.value] || [])
 
     const containerStyleClasses = [
         'container',
@@ -14,23 +22,16 @@
 </script>
 
 <template>
+  <div 
+    :class="containerStyleClasses" 
+    class="flex flex-row gap-8 flex-wrap justify-center"
+  >
 
-     <div 
-        :class="containerStyleClasses" 
-        class="flex flex-row gap-8 flex-wrap justify-center"
-    >
-
-        <resume-edu-content 
-            v-for="(edu, index) in educationsStore.data"
-            :key="index"
-            :educationLogoSrc="edu.educationLogoSrc"
-            :title="edu.title"
-            :issuer="edu.issuer"
-            :year="edu.year"
-            :certificationLink="edu.certificationLink"
-            :courseDetails="edu.courseDetails"
-        ></resume-edu-content>
+    <resume-edu-content 
+      v-for="(edu, index) in educations"
+      :key="index"
+      v-bind="edu"
+    ></resume-edu-content>
 
   </div>
-
 </template>
