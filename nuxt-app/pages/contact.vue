e7e01e71d2e7e01aef2626ddc49212677d3e2759
Dylan Tettarasar <dtettarasar@gmail.com>
2026-06-24 12:00:26 +0200

    contact page: update the page to add translations

--- nuxt-app/pages/contact.vue
+++ nuxt-app/pages/contact.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-slate-800">
     
-    <sections-hero-small icon="mdi:comment-account" title="Contact" ></sections-hero-small>
+    <sections-hero-small icon="mdi:comment-account" :title="pageTitle" ></sections-hero-small>
 
     <other-teasing></other-teasing>
 
@@ -9,5 +9,16 @@
 </template>
 
 <script setup lang="ts">
+
+import { computed } from 'vue'
+import { useI18n } from 'vue-i18n'
+import { navLabels } from '~/constants/ui-labels'
+
+const { locale } = useI18n()
+
+const pageTitle = computed(() => {
+  const lang = locale.value === 'fr' ? 'fr' : 'en'
+  return navLabels.contact[lang]
+})
 
 </script>
